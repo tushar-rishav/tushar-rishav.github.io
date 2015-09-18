7c96b15df954c5ac38baecc2e16db95425ad4381
Tushar Gautam <tushar.rishav@gmail.com>
2015-09-18 18:37:01 +0530

    pagination and global search added to blog

--- blog/app/controllers.js
+++ blog/app/controllers.js
@@ -1,14 +1,24 @@
 angular.module('app.controllers', [
 	'app.directives'
 ])
-	.controller('blogController', ['$scope', '$http', function($scope, $http){
+	.controller('blogController', ['$scope','$http', function($scope,$http){
+		$scope.currentPage = 1;
+		$scope.pageSize = 3;
 		$http.get('data/blog_detail.json').success(function(data){
 			$scope.posts = data;
-			
+			$scope.numPages = function() {
+		        return Math.ceil($scope.posts.length / $scope.pageSize);
+		    };
 		});
 	}])
 	.controller('overviewController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
 		$http.get('data/blog_detail.json').success(function(data){
 			$scope.post = data[$routeParams.id];
 		});
-	}]);
+	}])
+	.filter('startFrom', function() {	// custom filter for pagination
+    return function(input, start) {
+        start = +start; //parse to int
+        return input.slice(start);	
+    }
+	});
