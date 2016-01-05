2b251ed94adeb18322c105fceb319b733871fc0a
Tushar Gautam <tushar.rishav@gmail.com>
2016-01-05 19:00:43 +0530

    improved loading

--- js/progress_bar.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){var getMax=function(){return($(document).height()-$(window).height());}
-var getValue=function(){return $(window).scrollTop();}
-if('max'in document.createElement('progress')){var progressBar=$('progress');progressBar.attr({max:getMax()});$(document).on('scroll',function(){progressBar.attr({value:getValue()});});$(window).resize(function(){progressBar.attr({max:getMax(),value:getValue()});});}else{var progressBar=$('.progress-bar'),max=getMax(),value,width;var getWidth=function(){value=getValue();width=(value/max)*100;width=width+'%';return width;}
-var setWidth=function(){progressBar.css({width:getWidth()});}
-$(document).on('scroll',setWidth);$(window).on('resize',function(){max=getMax();setWidth();});}});
