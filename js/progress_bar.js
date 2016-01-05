e4e7db71522378f262ef6136421204d3b8de000f
Tushar Gautam <tushar.rishav@gmail.com>
2016-01-05 17:00:38 +0530

    optimised

--- js/progress_bar.js
+++ js/progress_bar.js
@@ -1,52 +1,5 @@
-$(document).ready(function(){
-    
-    var getMax = function(){
-        //console.log($(document).height()+","+$(window).height());
-        return ($(document).height() - $(window).height());
-    }
-    
-    var getValue = function(){
-        return $(window).scrollTop();
-    }
-    
-    if('max' in document.createElement('progress')){
-        // Those browser which supports progress element
-        var progressBar = $('progress');
-        
-        // Set the Max attr for the first time
-        progressBar.attr({ max: getMax() });
-
-        $(document).on('scroll', function(){
-            // On scroll only Value attr needs to be calculated
-            progressBar.attr({ value: getValue() });
-        });
-      
-        $(window).resize(function(){
-            progressBar.attr({ max: getMax(), value: getValue() });
-        });   
-    }
-    else {
-        /* For Older browser */
-        var progressBar = $('.progress-bar'), 
-            max = getMax(), 
-            value, width;
-        
-        var getWidth = function(){
-            // Get the width in percentage
-            value = getValue();            
-            width = (value/max) * 100;
-            width = width + '%';
-            return width;
-        }
-        
-        var setWidth = function(){
-            progressBar.css({ width: getWidth() });
-        }
-        
-        $(document).on('scroll', setWidth);
-        $(window).on('resize', function(){
-            max = getMax();
-            setWidth();
-        });
-    }
-});
+$(document).ready(function(){var getMax=function(){return($(document).height()-$(window).height());}
+var getValue=function(){return $(window).scrollTop();}
+if('max'in document.createElement('progress')){var progressBar=$('progress');progressBar.attr({max:getMax()});$(document).on('scroll',function(){progressBar.attr({value:getValue()});});$(window).resize(function(){progressBar.attr({max:getMax(),value:getValue()});});}else{var progressBar=$('.progress-bar'),max=getMax(),value,width;var getWidth=function(){value=getValue();width=(value/max)*100;width=width+'%';return width;}
+var setWidth=function(){progressBar.css({width:getWidth()});}
+$(document).on('scroll',setWidth);$(window).on('resize',function(){max=getMax();setWidth();});}});
